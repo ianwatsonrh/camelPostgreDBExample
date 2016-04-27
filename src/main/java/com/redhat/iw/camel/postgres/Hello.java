d6f1ef3aa96c5bbbbd9484aec14a74af1e86d356
Ian Watson <ianwatson@ianwatson-OSX.local>
2016-04-27 14:35:53 +0100

    Adding source

---
+++ src/main/java/com/redhat/iw/camel/postgres/Hello.java
@@ -0,0 +1,10 @@
+package com.redhat.iw.camel.postgres;
+
+/**
+ * An interface for implementing Hello services.
+ */
+public interface Hello {
+
+    String hello();
+	
+}
---
+++ src/main/java/com/redhat/iw/camel/postgres/SQLBean.java
@@ -0,0 +1,12 @@
+package com.redhat.iw.camel.postgres;
+
+/**
+ * A bean which we use in the route
+ */
+public class SQLBean {
+
+    public String hello() {
+        return "select * from customer;";
+    }
+
+}
